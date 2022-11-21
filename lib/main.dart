f0770f12b90354cfed1136735e0c3922c920d515
Ashish131294 <ashish.kachhadiya1114@gmail.com>
2022-11-21 08:45:28 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+
+void main()
+{
+  runApp(MaterialApp(home: demo(),debugShowCheckedModeBanner: false,));
+}
+
+class demo extends StatefulWidget {
+  const demo({Key? key}) : super(key: key);
+
+  @override
+  State<demo> createState() => _demoState();
+}
+
+class _demoState extends State<demo> {
+  int _counter = 0;
+
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Razorpay"),),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Pay with Razorpay demo',
+            ),
+            ElevatedButton(onPressed: (){
+              Razorpay razorpay = Razorpay();
+              var options = {
+                'key': 'rzp_test_z5hWh1DGkwUwjg',
+                'amount': 100,
+                'name': 'Acme Corp.',
+                'description': 'Fine T-Shirt',
+                'retry': {'enabled': true, 'max_count': 1},
+                'send_sms_hash': true,
+                'prefill': {'contact': '8888888888', 'email': 'test@razorpay.com'},
+                'external': {
+                  'wallets': ['paytm']
+                }
+              };
+              razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlePaymentErrorResponse);
+              razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlePaymentSuccessResponse);
+              razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handleExternalWalletSelected);
+              razorpay.open(options);
+            },
+                child: const Text("Razorpay")),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void handlePaymentErrorResponse(PaymentFailureResponse response){
+    /*
+    * PaymentFailureResponse contains three values:
+    * 1. Error Code
+    * 2. Error Description
+    * 3. Metadata
+    * */
+    showAlertDialog(context, "Payment Failed", "Code: ${response.code}\nDescription: ${response.message}\nMetadata:${response.error.toString()}");
+  }
+
+  void handlePaymentSuccessResponse(PaymentSuccessResponse response){
+    /*
+    * Payment Success Response contains three values:
+    * 1. Order ID
+    * 2. Payment ID
+    * 3. Signature
+    * */
+    showAlertDialog(context, "Payment Successful", "Payment ID: ${response.paymentId}");
+  }
+
+  void handleExternalWalletSelected(ExternalWalletResponse response){
+    showAlertDialog(context, "External Wallet Selected", "${response.walletName}");
+  }
+
+  void showAlertDialog(BuildContext context, String title, String message){
+    // set up the buttons
+    Widget continueButton = ElevatedButton(
+      child: const Text("Continue"),
+      onPressed:  () {},
+    );
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text(title),
+      content: Text(message),
+      actions: [
+        continueButton,
+      ],
+    );
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+}
